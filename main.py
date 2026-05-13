27856acb5d707b360cc08cb7a60b8b28e61314e5
6f9 <151858999+rsmdevlin@users.noreply.github.com>
2026-05-13 03:54:28 +0200

    Update main.py

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ from telegram.ext import (
 import google.generativeai as genai
 
 TELEGRAM_TOKEN = os.environ.get("TELEGRAM_TOKEN", "")
-GEMINI_API_KEY = os.environ.get("GEMINI_API_KEY", "AIzaSyCYsbzzp36yoUw8X4C0GNhAyWP25sG-2Cs")
+GEMINI_API_KEY = os.environ.get("GEMINI_API_KEY", "AIzaSyDpg8K2RbbVuF7aUVeiakqmJ3abVHfygNI")
 
 genai.configure(api_key=GEMINI_API_KEY)
 
